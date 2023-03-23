bf2da934d80a3550d8772ef03618a2cf3c098b8b
Rohan Verma <rv725555@gmail.com>
2023-03-23 20:08:22 +0530

    feat: Added a button that controls rendering.

--- three-js-book/world-app/src/main.js
+++ three-js-book/world-app/src/main.js
@@ -1,8 +1,21 @@
 // To do...
 import World from './World/World.js';
 
-const container = document.querySelector('#scene-container');
+function main() {
+    const container = document.querySelector('#scene-container');
 
-const world = new World(container);
+    const world = new World(container);
 
-world.render();
+    world.render();
+}
+
+const button = document.createElement("button");
+
+button.innerText = "Render Cube";
+
+button.onclick = main;
+
+document.querySelector("body").append(button);
+
+
+
