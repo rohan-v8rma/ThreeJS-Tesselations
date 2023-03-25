33c5ece47a9c1c5ceb28874baa1f2b69ac85c792
Rohan Verma <rv725555@gmail.com>
2023-03-25 12:24:08 +0530

    chore: Boilerplate for adding DirectionalLight.

---
+++ three-js-book/world-app/src/World/components/lights.js
@@ -0,0 +1,12 @@
+// Adding a DirectionalLight to the scene...
+//TODO: Read about it here - https://discoverthreejs.com/book/first-steps/physically-based-rendering/#introducing-the-directionallight
+
+import { DirectionalLight } from 'three';
+
+function createLights() {
+    const light = null;
+
+    return light;
+}
+
+export default createLights;
