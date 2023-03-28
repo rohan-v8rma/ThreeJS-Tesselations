237fd9c7bf3123c5fd106e54719c538aa3f7df1d
Rohan Verma <rv725555@gmail.com>
2023-03-28 11:25:10 +0530

    test: Used controls.target and controls.enablePan properties.

--- three-js-book/world-app/src/World/World.js
+++ three-js-book/world-app/src/World/World.js
@@ -26,9 +26,7 @@ class World {
   constructor(container) {
     camera = createCamera(0, 0, 15);
     renderer = createRenderer();
-    scene = createScene();
-
-    const controls = createControls(camera, renderer.domElement);
+    scene = createScene();  
 
     // Objects that just need to be added into the scene are made as local to the scope of the constructor.
     //! We do NOT need to acess the below defined variables once they have been added into the scene
@@ -37,7 +35,14 @@ class World {
     const light = createLights(0, 3, 3, 0, 0, 0);
     const lightHelper = new DirectionalLightHelper(light, 5);
     
-    
+    const controls = createControls(camera, renderer.domElement);
+    //* By default, the controls orbit around the center of the scene, point (0,0,0). This is stored in the controls.target property.
+    // controls.target.set(10, 20, 30); // Making controls to orbit a point in world space
+    controls.target.copy(cube.position); // Making controls to orbit the cube
+
+    //? Panning the controls using right mouse button pans the target too. Can be disabled by setting enablePan to false.
+    controls.enablePan = false;
+
     container.append(renderer.domElement);
 
     //!NOTE: Added the light and the mesh in a single call of scene.add. We can add as many objects as we like, separated by commas.
