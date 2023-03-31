2aa4c3377aa1549d74881e5b3611b9ab1c67421f
Rohan Verma <rv725555@gmail.com>
2023-03-31 06:58:25 +0530

    test: Using 'name' property of Object3D class for referencing.

--- three-experimentation/src/main.js
+++ three-experimentation/src/main.js
@@ -14,6 +14,32 @@ const canvas = document.getElementById('scene-container')
 canvas.appendChild(renderer.domElement);
 
 //-------- ----------
+// CREATING OBJECTS WITH NAMES
+//-------- ----------
+const PREFIX = 'box';
+const COLORS = ['red', 'blue', 'green'];
+const COUNT = COLORS.length;
+let i = 0;
+while(i < COUNT){
+    const geo = new THREE.BoxGeometry();
+    const mat = new THREE.MeshBasicMaterial();
+    const mesh = new THREE.Mesh(geo, mat);
+    mesh.name = PREFIX + i;
+    scene.add(mesh)
+    i += 1;
+}
+//-------- ----------
+// USING GET By NAME TO GET REFERNCE TO OBJECTS
+//-------- ----------
+COLORS.forEach((colorStr, i) => {
+    const mesh = scene.getObjectByName(PREFIX + i);
+    const a_mpos = i  / ( COUNT - 1 );
+    if(mesh){
+        mesh.position.x = -5 + 10 * a_mpos;
+        mesh.material.color = new THREE.Color(colorStr);
+    }
+});
+//-------- ----------
 // RENDER
 //-------- ----------
 camera.position.set(10, 10, 10);
